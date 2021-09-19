7934706bc8ade9f8a205afca6a62397955b797da
kokoaono <nyswzt.a@gmail.com>
2021-09-19 12:37:02 +1200

    useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,36 +2,52 @@ import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
-  
-  useEffect(() => {
-    const search = async() => {
+
+  useEffect (() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect (() => {
+    const search = async () => {
       const {data} =  await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
+
       setResults(data.query.search);    
     };
-
-    if (term){
-      search();
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+           className="ui button"
+           href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go</a>
+        </div>
         <div className="content">
           <div className="header">
             {result.title}
           </div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
         </div>
       </div>
     );
